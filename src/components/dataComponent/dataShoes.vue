b82e5b94f1a8499ee3c109c5d73c9f6fea9b2f52
ali.mpp <alimpp766@gmail.com>
2022-07-11 11:18:59 +0430

    Change router link location

--- src/components/dataComponent/dataShoes.vue
+++ src/components/dataComponent/dataShoes.vue
@@ -4,21 +4,24 @@
         <loading v-if="loading"/>
         <div v-else class="app-flex-wrap app-flex-justify-content opa-anim"  style="width:100%;">
         <div class="app-cart my-2 mx-2 pointer" v-for="data in getShoes" :key="data.id">
-           <router-link :to="`/shoes/${data.id}`" class="app-flex-wrap app-flex-justify-content link">
+           <router-link :to="`/shoes/${data.id}`" class="app-flex-justify-content link">
                 <div class="app-cart-img">
                 <img :src="data.image" class="app-img-cart" alt="error">
                 </div>
+           </router-link>
                 <div class="app-cart-text px-3 app-flex-justify-content">
+                  <router-link :to="`/shoes/${data.id}`" class="app-flex-column link">
                     <span class="app-text-size-y app-light-color">Product Name :</span>
                     <span class="app-text-size-y app-light-color">{{data.name}}</span>
                     <span class="app-text-size-y app-light-color">Size :</span>
                     <span class="app-text-size-y app-light-color">{{data.size}}</span>
                     <span class="app-text-size-x app-light-color">{{data.price}} $</span>
+                  </router-link>
                     <div> 
                     <button class="app-btn-blue mt-2">Add To Cart</button>
                     </div>
                 </div>
-           </router-link>
+       
         </div>
       </div>
      </div>
