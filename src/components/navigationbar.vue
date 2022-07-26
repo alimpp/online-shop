b36b5c8540a1d7ffcc5cedc56bebec2bb9e6bc65
ali.mohsenpour <alimpp766@gmail.com>
2022-07-26 17:06:39 +0430

    Make Change

--- src/components/navigationbar.vue
+++ src/components/navigationbar.vue
@@ -2,7 +2,7 @@
   <div class="navigationbar">
     <nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" style="color:#A084CF;">SHOPING CART</a>
+    <a class="navbar-brand" style="color:#A084CF;">SHOPPING CART</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
      <i class="bi bi-list app-light-color app-text-size-b"></i>
     </button>
