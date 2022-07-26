179c18335530fc5eda165d77c4597574d16cc1f3
ali.mohsenpour <alimpp766@gmail.com>
2022-07-26 14:25:02 +0430

    Make Chnage in badge

--- src/components/navigationbar.vue
+++ src/components/navigationbar.vue
@@ -2,7 +2,7 @@
   <div class="navigationbar">
     <nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" style="color:#A084CF;">ONLINE SHOP</a>
+    <a class="navbar-brand" style="color:#A084CF;">SHOPING CART</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
      <i class="bi bi-list app-light-color app-text-size-b"></i>
     </button>
@@ -27,10 +27,13 @@
           <router-link to="/sports" class="link mx-2 py-1 app-light-color">Sports</router-link>
         </li>
          <li class="nav-item">
-          <router-link to="/cart" class="link mx-2 py-1 app-light-color"><i class="bi bi-bag-plus-fill"></i></router-link>
-          <span class="badge app-red-bg">{{getCount}}</span>
+         
         </li>
       </ul>
+      <form class="d-flex">
+       <div> <router-link to="/cart" class="link"><i class="bi bi-bag-plus-fill app-light-color"  style="font-size:30px;"></i></router-link></div>
+       <div class="mb-2 mx-1"> <span class="badge app-red-bg">{{getCount}}</span></div>
+      </form>
     </div>
   </div>
 </nav>
@@ -38,7 +41,9 @@
 </template>
 
 <script>
+import coreCart from './coreCart.vue'
 export default {
+  components : {coreCart} ,
   computed : {
     getCount(){
       return this.$store.getters['CartStore/getCount']
